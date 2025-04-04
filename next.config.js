6e38585c844d83226ce8cc0cccdda343cb4367d9
Abraham Imohiosen <abrahamimohiosen@gmail.com>
2025-04-04 02:23:41 +0200

    Refactor ESLint configuration and add initial Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any other configurations as needed
+}
+
+module.exports = nextConfig
